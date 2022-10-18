fb32d67a796c527d99db0eee0010a72690451730
Prashant Govindarajan <37257549+gprashant17@users.noreply.github.com>
2022-10-17 23:24:17 -0400

    Update __init__.py

--- crystal_design/envs/__init__.py
+++ crystal_design/envs/__init__.py
@@ -1,2 +1,21 @@
 from crystal_design.envs.seh_frag_molecule_env import SEHFragmentMoleculeEnvironment
-from crystal_design.envs.crystal_env import CrystalEnv
+# from crystal_design.envs.crystal_env import CrystalEnv
+from hive.envs.base import BaseEnv, ParallelEnv
+from hive.envs.env_spec import EnvSpec
+from hive.envs.gym_env import GymEnv
+from hive.utils.registry import registry
+    
+try:
+    from crystal_design.envs.crystal_env import CrystalEnv
+except ImportError:
+    CrystalEnv = None
+
+registry.register_all(
+    BaseEnv,
+    {
+        "GymEnv": GymEnv,
+        "CrystalEnv": CrystalEnv
+    },
+)
+
+get_env = getattr(registry, f"get_{BaseEnv.type_name()}")
