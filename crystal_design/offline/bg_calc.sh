c91348472b50e7d167f99cf5c48688b8d46f8867
gprashant17 <prashantgovind1999@gmail.com>
2023-09-20 00:38:15 -0400

    more ignored files

--- crystal_design/offline/bg_calc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1362..27135}; 
-    do 
-        mgl predict -m MEGNet-MP-2019.4.1-BandGap-mfi --infile mp_20_cifs/$i.cif > matgl_pred_bg_mp_train/$i.txt
-    done
--- crystal_design/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from crystal_design.utils.utils import *
-from crystal_design.utils.data_utils import *
-# from crystal_design.utils.converters.pyg_graph_to_tensor import *
-
