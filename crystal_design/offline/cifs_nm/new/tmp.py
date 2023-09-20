d969ec61b40ef762b5676aab375c25c8363ac4b3
gprashant17 <prashantgovind1999@gmail.com>
2023-09-20 02:55:44 -0400

    modified gitignore

--- crystal_design/offline/cifs_nm/new/tmp.py
+++
@@ -1,5 +0,0 @@
-import sys
-if __name__=='__main__':
-    a = sys.argv
-    print(a)
-
--- crystal_design/offline/pd_to_cif.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from tqdm import tqdm
-
-data = pd.read_csv('/home/mila/p/prashant.govindarajan/scratch/crystal_design_project/crystal-design/crystal_design/data/mp_20/train.csv')
-
-for i in tqdm(range(len(data))):
-    cif_string = data.loc[i]['cif']
-    with open('mp_20_cifs/'+str(i)+'.cif', 'w') as f:
-        f.write(cif_string)
