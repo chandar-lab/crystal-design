fac6cffe62d6440bc1367de389bf3942fc5c42c1
Prashant Govindarajan <37257549+gprashant17@users.noreply.github.com>
2023-09-21 01:47:11 -0400

    Update launcher.sh

--- crystal_design/runner/launcher.sh
+++ crystal_design/runner/launcher.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 ​
-for weight in 5
+si_bg=1.12
+for weight in 1 5
 do
     for beta in 1 3
     do
         for alpha2 in 5 10
         do
-            for alpha1 in 1
+            for alpha1 in 1 0
             do
-                name="112megnet-MG-w"$weight-$alpha1-$alpha2-$beta
-                sbatch script $alpha1 $alpha2 $beta $weight $name
+                name="112megnet-MG-w"$weight-$alpha1-$alpha2-$beta ## IF BG = 1.12
+                # name="megnet-MG-w"$weight-\($alpha1-$alpha2-$beta\)"-bg4-nq1"   ## IF BG = 4.0
+                sbatch script $alpha1 $alpha2 $beta $weight $name $si_bg
             done
         done
     done
